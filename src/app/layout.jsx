206e53a753c7f25c2d360c6eece7674d625b9526
KimSeongeun <elex10147356@gmail.com>
2025-05-28 15:40:33 +0900

    리치텍스트에디터 뺌 (너무신경쓸게많아염ㅋㅋ)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,9 +11,6 @@ export default function layout({children}) {
         <head>
             <meta charSet="utf-8" />
             <title>EatPlan</title>
-            <link rel="stylesheet" href="/richtexteditor/rte_theme_default.css" />
-            <script type="text/javascript" src="/richtexteditor/rte.js"></script>
-            <script type="text/javascript" src='/richtexteditor/plugins/all_plugins.js'></script>
         </head>
         <body>
         {children}
--- src/app/write/courseWrite.jsx
+++ src/app/write/courseWrite.jsx
@@ -1,31 +1,21 @@
 'use client'
-import {useEffect, useRef, useState} from "react";
+import './courseWriteCss.css'
 
 export default function CourseWrite() {
-    let div = useRef(null);
-    const [editorInstance, setEditorInstance] = useState(null);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined' && window.RichTextEditor && div.current) {
-            const editor = new window.RichTextEditor(div.current);
-            setEditorInstance(editor);
-        }
-    }, []);
-
     return (
         <>
             <div className={"rightMenu"}>
-                <table>
+                <table className={"courseWrite_table"}>
                     <tbody>
                     <tr>
                         <td className={"courseWrite_td"}>제목</td>
                         <td className={"courseWrite_td"}><input/></td>
                     </tr>
                     <tr>
-                        <td colSpan={2}>코스 등록</td>
+                        <td colSpan={2} className={"courseWrite_td"}>코스 등록</td>
                     </tr>
                     <tr>
-                        <td colSpan={2}><input/></td>
+                        <td colSpan={2} className={"courseWrite_td"}><input/></td>
                     </tr>
                     </tbody>
                 </table>
@@ -33,24 +23,33 @@ export default function CourseWrite() {
                 <table>
                     <tbody>
                     <tr>
-                        <td colSpan={2}>지도 상세보기 (1개 이상의 코스가 등록될 시 자동으로 추가됩니다.)</td>
+                        <td colSpan={2} className={"courseWrite_td"}>지도 상세보기 (1개 이상의 코스가 등록될 시 자동으로 추가됩니다.)</td>
                     </tr>
                     <tr>
-                        <td colSpan={2}>개인 코멘트 입력</td>
+                        <td colSpan={2} className={"courseWrite_td"}></td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2} className={"courseWrite_td"}>개인 코멘트 입력</td>
                     </tr>
                     <tr>
                         <td colSpan={2}>
-                            <div className={"writeForm"} ref={div}></div>
+                            {/*<div className={"writeForm"} ref={div}></div>*/}
+                            <textarea/>
                         </td>
                     </tr>
                     <tr>
-                        <td>공개 및 비공개 여부</td>
-                        <td><input type={"radio"}/>공개&nbsp;<input type={"radio"}/>비공개</td>
+                        <td className={"courseWrite_td"}>공개 및 비공개 여부</td>
+                        <td className={"courseWrite_td"}>
+                            <input type={"radio"}/>공개&nbsp;
+                            <input type={"radio"}/>비공개
+                        </td>
                     </tr>
                     <tr>
-                        <td colSpan={2}>태그 (최대 5개까지 선택 가능)</td>
+                        <td colSpan={2} className={"courseWrite_td"}>태그 (최대 5개까지 선택 가능)</td>
                     </tr>
-                    <tr><td colSpan={2}></td></tr>
+                    <tr>
+                        <td colSpan={2} className={"courseWrite_td"}></td>
+                    </tr>
                     </tbody>
                 </table>
                 <button>임시 저장</button>
