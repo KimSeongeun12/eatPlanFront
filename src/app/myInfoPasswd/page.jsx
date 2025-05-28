ebc382fc8ac0e1293418ed0ddd2508ff3ed41b28
KimSeongeun <elex10147356@gmail.com>
2025-05-28 10:35:04 +0900

    ㅁㄴㅇㄹ

--- src/app/myInfoPasswd/page.jsx
+++ src/app/myInfoPasswd/page.jsx
@@ -1,13 +1,8 @@
 'use client'
 import LeftMenu from "@/app/leftMenu";
-<<<<<<< HEAD:src/app/passwd/myInfoPasswd/page.jsx
-import '../../mainCss.css'
-import '../../mypage/myPageCss.css';
-import MyInfoPasswd from "@/app/passwd/myInfoPasswd/myInfoPasswd";
-=======
+import '../mainCss.css'
 import '../mypage/myPageCss.css';
 import MyInfoPasswd from "./myInfoPasswd";
->>>>>>> origin/master:src/app/myInfoPasswd/page.jsx
 
 export default function PasswdPage() {
     return (
