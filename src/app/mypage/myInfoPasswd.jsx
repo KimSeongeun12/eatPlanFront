5b94b52c90667c4fcba622e79f9883a23ba8a0f3
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-05-28 09:54:54 +0900

    회원정보 수정 사이 비번확인

---
+++ src/app/mypage/myInfoPasswd.jsx
@@ -0,0 +1,56 @@
+'use client'
+
+import {useRouter} from "next/navigation";
+import axios from "axios";
+import {useState} from "react";
+
+export default function MyInfoPasswd() {
+
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const checkPassword = async () => {
+        if (!password) {
+            alert('비밀번호를 입력해주세요.');
+            return;
+        }
+        setLoading(true);
+
+        try {
+            const {data : verify} = await axios.post('http://localhost/member_pass',{password});
+
+            if(!verify.success) {
+                alert('비밀번호가 일치하지 않습니다.');
+                return;
+            }
+                //비밀번호가 맞으면 회원정보 수정 페이지로 이동
+                router.push('/myinfo_update');
+            }catch (err){
+                console.log(err);
+                alert('오류가 발생했습니다.');
+            }finally{
+                setLoading(false);
+            }
+
+    };
+
+    return (
+        <div className="rightMenu">
+            <h3 className="passwd-title">비밀번호 확인</h3>
+            <div className="Mypage_passwd_div">
+                <input
+                    type="password" placeholder="비밀번호를 입력해주세요"
+                    value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <p>회원 정보 수정을 위한 비밀번호 확인 절차입니다.</p>
+                <button
+                    onClick={checkPassword}
+                    disabled={loading}
+                    className="infoUpdateButton"
+                >
+                    {loading ? '처리중...' : '비밀번호 확인'}
+                </button>
+            </div>
+        </div>
+    );
+}
