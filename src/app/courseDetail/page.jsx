7c44b43c057432543045a8ea6b429ddc312d4c55
kwan_hyung_lee <grick21@gmail.com>
2025-05-29 18:18:59 +0900

    css 수정중

--- src/app/courseDetail/page.jsx
+++ src/app/courseDetail/page.jsx
@@ -1,8 +1,19 @@
 'use client'
 
+import LeftMenu from "@/app/leftMenu";
+import "./courseDetail.css";
+import CourseDetail from "@/app/courseDetail/courseDetail";
+
 export default function CourseDetailPage() {
 
     return (
-        <></>
+        <>
+            <div className={"pageContainer"}>
+                <LeftMenu />
+                <div className={"rightMenu"}>
+                    <CourseDetail />
+                </div>
+            </div>
+        </>
     );
 }
