3ae70249a5018aeb00046a4c0ef8c704ad7de866
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-06-02 16:50:31 +0900

    no message

--- src/app/noticeWrite/page.jsx
+++ src/app/noticeWrite/page.jsx
@@ -1,24 +1,15 @@
 'use client';
 
-import React from 'react';
-import LeftMenu from '@/app/leftMenu';
-import NoticeWrite from './noticeWrite';
-import './noticeWrite.css';
-import { Box } from '@mui/material';
+import LeftMenu from "@/app/leftMenu";
+import NoticeWrite from "./noticeWrite";
 
-export default function NoticeWritePage() {
+export default function notice() {
     return (
+        <>
             <LeftMenu />
-            {/* 우측 메인 콘텐츠: flex로 가운데 정렬 및 너비 제한 */}
-            <div className="notice-write-right">
-                <Box
-                    sx={{
-                        maxWidth: 700,
-                        width: '100%',
-                        margin: '0 auto',
-                    }}>
-                    <NoticeWrite />
-                </Box>
+            <div className="rightMenu">
+                <NoticeWrite />
             </div>
+        </>
     );
 }
