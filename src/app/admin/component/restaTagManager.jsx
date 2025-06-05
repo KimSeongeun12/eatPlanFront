48a9f5283c03643fc8fbdff372b60b8fe22f186a
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-06-05 16:59:21 +0900

    Update restaTagManager.jsx
    한발자국씩느림

--- src/app/admin/component/restaTagManager.jsx
+++ src/app/admin/component/restaTagManager.jsx
@@ -15,7 +15,7 @@ export default function RestaTagManager({tags, resta_idx}) {
 
     useEffect(() => {
         drawTags();
-    }, [resta_idx]);
+    },[resta_idx]);
 
     const [list, setList] = useState([]);
 
@@ -28,12 +28,11 @@ export default function RestaTagManager({tags, resta_idx}) {
     // 태그들을 그리는 함수
     const drawTags = () => {
         const data = tags.map((item) => {
-            console.log(item);
             return (
                 <div key={item.tag_idx}
                      style={{border: "1px solid lightblue", padding: "3px", margin: "3px", width: "100px"}}>
                     {item.tag_name}
-                    <span style={{color: "red"}} onClick={() => delTag(item.tag_idx)}>X</span>
+                    <span style={{color: "red", cursor:"pointer"}} onClick={() => delTag(item.tag_idx)}>X</span>
                 </div>
             );
         });
@@ -44,31 +43,40 @@ export default function RestaTagManager({tags, resta_idx}) {
         <div>
             {/*태그 출력 부분*/}
             <div>{list}</div>
-            <TagInput resta_idx={resta_idx}/>
+            <TagInput resta_idx={resta_idx} drawTags={drawTags}/>
         </div>
     );
 }
 
 
 // 태그 입력 컴포넌트
-function TagInput({resta_idx}) {
+function TagInput({resta_idx, drawTags}) {
 
     const [options, setOptions] = useState([]);
+    const [input, setInput] = useState('');
 
     useEffect(() => {
         makeOptions();
     }, [resta_idx]);
 
-    const insert = () => {
-        
+    const insert = async () => {
+        let {data} = await axios.post(`http://localhost/addTagtoResta`, {tag_idx: input, resta_idx: resta_idx});
+        drawTags();
     }
-    // 태그리스트 전체를 불러와서 추가해야함(isClass : 식당)
+
+    // ----------------- 귀찮으니 엔터로 해결! -------------//
+    const enterHandler = (e) => {
+        if (e.key === 'Enter') {
+            insert();
+        }
+    }
 
     const makeOptions = async () => {
         let {data} = await axios.get(`http://localhost/list_tag`);
         const list = data.list_tag_whole.map((item) => {
-            if(item.isClass==='식당'){
+            if (item.isClass === '식당') {
                 return (
+                    // value는 tag_idx로 받음
                     <option value={item.tag_idx}>{item.tag_name}</option>
                 )
             }
@@ -79,10 +87,16 @@ function TagInput({resta_idx}) {
     return (
         <div style={{display: "flex", alignItems: "center"}}>
             {/*select문으로 받을예정*/}
-            <select name={"tags"} style={{height:'30px'}}>
+            <select style={{height: '30px'}}
+                    name={"tags"}
+                    onKeyUp={() => enterHandler()}
+                    onChange={(e) => setInput(e.target.options[e.target.selectedIndex].value)}>
                 {options}
             </select>
-            <div style={{border: "1px solid red", padding: "5px", cursor: "pointer", margin:"5px"}}>추가</div>
+            <div style={{border: "1px solid red", padding: "5px", cursor: "pointer", margin: "5px"}}
+                 onClick={() => insert()}>
+                추가
+            </div>
         </div>
     );
 }
