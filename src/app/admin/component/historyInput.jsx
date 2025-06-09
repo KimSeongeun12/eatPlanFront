c59d5cb28f24686edcc59ffdc4de6980d92e89c4
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-06-09 17:16:34 +0900

    히스토리 추가 완료

---
+++ src/app/admin/component/historyInput.jsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {useState} from "react";
+import axios from "axios";
+
+export default function HistoryInput({report_idx, drawList}){
+
+    const [input, setInput] = useState('');
+    let user_id=sessionStorage.getItem("user_id");
+
+    //히스토리 추가
+    const insert=async ()=>{
+        let {data}=await axios.post(`http://localhost/history_write`, {report_idx: report_idx, user_id: user_id, content: input});
+        if(data.success){
+            drawList();
+            setInput('');
+        }
+    }
+
+    //엔터
+    const enterHandler=(e)=>{
+        if(e.key === 'Enter'){
+            insert();
+        }
+    }
+
+    return(
+        <div>
+            <input type={"text"}
+                   style={{height:"30px", border:"1px solid black", marginBottom:"10px", marginTop:"10px"}}
+                   value={input}
+                   onKeyUp={enterHandler}
+                   onChange={(e) => setInput(e.target.value)}
+                   placeholder={"내용을 입력하세요"}
+            />
+        </div>
+    );
+}
