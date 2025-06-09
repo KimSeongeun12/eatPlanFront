56ac4adf19484c6328258acc8e5cf2eef62a0f2e
kwan_hyung_lee <grick21@gmail.com>
2025-06-09 17:19:14 +0900

    관리자 권한 정상화

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -7,7 +7,6 @@ export default function LoginPage() {
     useEffect(() => {
         const user_id = sessionStorage.getItem("user_id");
         const token = sessionStorage.getItem("token");
-        const admin=false;
 
         if (user_id != null) {
             sessionStorage.removeItem("user_id");
@@ -61,7 +60,7 @@ export default function LoginPage() {
             alert("로그인에 성공했습니다.");
             sessionStorage.setItem('user_id', data.user_id);
             sessionStorage.setItem('token', data.token);
-            sessionStorage.setItem('isAdmin', data.admin === 1? '1' : '0');
+            sessionStorage.setItem('admin', data.admin);
             location.href="/list";
         } else {
             alert("아이디 또는 비밀번호를 확인해주세요.");
