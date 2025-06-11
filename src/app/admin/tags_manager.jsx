7a8ddeb8096a1cd053c927af9e30b0e1500fa81b
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-06-11 10:56:03 +0900

    코스/식당 태그관리css수정

--- src/app/admin/tags_manager.jsx
+++ src/app/admin/tags_manager.jsx
@@ -7,9 +7,12 @@ import DrawResta from "@/app/admin/draw_resta";
 export default function TagsManager() {
 
     const [component, setComponent] = useState(null);
+    const [selected, setSelected] = useState(null);
 
     const toggle = (e) => {
         // console.log(e.target.id);
+        setSelected(e.target.id);
+
         switch (e.target.id) {
             case "course":
                 setComponent(<DrawLeftTags isClass={'course'} leftMenu={e.target.id}/>);
@@ -27,9 +30,18 @@ export default function TagsManager() {
         <div>
             <br/>
             <div className={"tag tab"}>
-                <div id={'course'} onClick={(e) => toggle(e)}>코스 태그 관리</div>
-                <div id={'restaurant'} onClick={(e) => toggle(e)}>식당 태그 관리</div>
-                <div id={'restaurant_tags'} onClick={(e) => toggle(e)}>식당 별 태그 관리</div>
+                <div id={'course'}
+                     onClick={(e) => toggle(e)}
+                     className={selected==='course'? 'selected':''}
+                >코스 태그 관리</div>
+                <div id={'restaurant'}
+                     onClick={(e) => toggle(e)}
+                     className={selected==='restaurant'? 'selected':''}
+                >식당 태그 관리</div>
+                <div id={'restaurant_tags'}
+                     onClick={(e) => toggle(e)}
+                     className={selected==='restaurant_tags'? 'selected':''}
+                >식당 별 태그 관리</div>
             </div>
             {component}
         </div>
